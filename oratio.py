b4503b14a983990ea1bcb70ffe079e9f3e98a409
Sam Ireland <mail@samireland.com>
2024-10-30 23:00:35 +0000

    Model model

--- oratio.py
+++ oratio.py
@@ -30,13 +30,36 @@ class User:
 
     def post(self, path, **kwargs):
         return self.request("POST", path, **kwargs)
+    
+
+    def models(self):
+        data = self.get("models")
+        return [Model(model["id"], model) for model in data["data"]]
+    
+
+    def model(self, id):
+        data = self.get(f"models/{id}")
+        return Model(data["id"], data)
 
 
     def complete(self, messages, model="gpt-3.5-turbo"):
+        model_name = model.name if isinstance(model, Model) else model
         return self.post(
             "chat/completions",
             json={
-                "model": model,
+                "model": model_name,
                 "messages": messages,
             }
         )
+
+
+
+class Model:
+
+    def __init__(self, id, json):
+        self.id = id
+        self.json = json
+    
+
+    def __repr__(self):
+        return f"Model({self.id})"
