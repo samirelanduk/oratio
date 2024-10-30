29b488c0cf27682d8864ee604e4fc1a337e51021
Sam Ireland <mail@samireland.com>
2024-10-30 22:55:00 +0000

    User request methods

--- oratio.py
+++ oratio.py
@@ -11,16 +11,32 @@ class User:
         return f"User({truncated_token})"
     
 
-    def complete(self, messages, model="gpt-3.5-turbo"):
-        response = requests.post(
-            "https://api.openai.com/v1/chat/completions",
+    def request(self, method, path, **kwargs):
+        response = requests.request(
+            method,
+            f"https://api.openai.com/v1/{path}",
             headers={
                 "Authorization": f"Bearer {self.token}",
                 "Content-Type": "application/json",
             },
+            **kwargs
+        )
+        return response.json()
+    
+
+    def get(self, path):
+        return self.request("GET", path)
+    
+
+    def post(self, path, **kwargs):
+        return self.request("POST", path, **kwargs)
+
+
+    def complete(self, messages, model="gpt-3.5-turbo"):
+        return self.post(
+            "chat/completions",
             json={
                 "model": model,
                 "messages": messages,
             }
         )
-        return response.json()
