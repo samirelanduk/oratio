d4ec4a9a72eaa6656862ecd65c5423c5ddea0bb7
Sam Ireland <mail@samireland.com>
2024-10-30 22:52:55 +0000

    User model

--- oratio.py
+++ oratio.py
@@ -1,15 +1,26 @@
 import requests
 
-def complete(token, messages, model="gpt-3.5-turbo"):
-    response = requests.post(
-        "https://api.openai.com/v1/chat/completions",
-        headers={
-            "Authorization": f"Bearer {token}",
-            "Content-Type": "application/json",
-        },
-        json={
-            "model": model,
-            "messages": messages,
-        }
-    )
-    return response.json()
+class User:
+
+    def __init__(self, token):
+        self.token = token
+    
+
+    def __repr__(self):
+        truncated_token = self.token[:13] + "..." + self.token[-5:]
+        return f"User({truncated_token})"
+    
+
+    def complete(self, messages, model="gpt-3.5-turbo"):
+        response = requests.post(
+            "https://api.openai.com/v1/chat/completions",
+            headers={
+                "Authorization": f"Bearer {self.token}",
+                "Content-Type": "application/json",
+            },
+            json={
+                "model": model,
+                "messages": messages,
+            }
+        )
+        return response.json()
